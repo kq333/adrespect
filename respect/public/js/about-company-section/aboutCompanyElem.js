c0800792e703abfe3b6125144bf295346ad9269c
kq333 <afugowski@yandex.com>
2023-08-20 09:32:18 +0200

    corrected image on mobile view in about company component

--- respect/public/js/about-company-section/aboutCompanyElem.js
+++ respect/public/js/about-company-section/aboutCompanyElem.js
@@ -3,10 +3,8 @@ import { aboutCompanyContent } from '../about-company-section/contentAboutCompan
 export const aboutCompanySection = aboutCompanyContent.map(HTMLelem => {
   return `
     <div class="lg:flex font-inter xl:gap-16 md:gap-8 bg-[#1B5B31] text-[#F5F0EC] block">
-      <div>
-        <div class="relative min-w-[688px] min-h-[720px]">
-          <img src="${HTMLelem.bigImage}" alt="image" class="absolute w-full h-full object-fit">
-        </div>
+      <div class="md:min-w-[688px] md:min-h-[720px] relative">
+        <img src="${HTMLelem.bigImage}" alt="image" class="w-full h-full object-cover">
       </div>
 
       <div class="py-24 xl:pl-24 md:pl-4 pl-6 pr-2">
